db1d448fdcb38fe6e1fef2d8f2123208de4f897a
“Aladser” <aladser@gmail.com>
2023-09-13 16:38:30 +0900

    мп

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
             @endif
 
             <div class="p-6 bg-white border-b border-gray-200 text-center display-5">
-                Основной контейнер содержания профиля. <br>
+                Основной контейнер домашней страницы. <br>
                 Заглушка на будущее.
             </div>
         </div>
