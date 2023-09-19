93d9526318a540eb2a850bfdb1a951538467af58
Aladser <aladser@gmail.com>
2023-09-19 15:18:29 +0900

    форма добавления оффера

--- resources/views/pages/advertiser.blade.php
+++ resources/views/pages/advertiser.blade.php
@@ -1,40 +1,59 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('app.profile') }}
-        </h2>
-    </x-slot>
+    <section class='w-50 mx-auto'>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Панель офферов</h2>
+        </x-slot>
 
-    <div class="py-12">
-        <div class="w-75 mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-4 bg-white border-b border-gray-200 text-center">
-                <section>
-                    <h3 class='h2 pb-3 fw-bolder'>Список офферов</h3>
-                    <table class='table w-75 mx-auto fs-4'>
-                        <tr>
-                            <th scope="col">Оффер</th>
-                            <th scope="col">Цена</th>
-                            <th scope="col">Статус</th>
-                            <th scope="col">Подписчики</th>
-                        </tr>
+        <article class='pt-4'>
+            <h3 class='h3 fw-bolder m-0 text-center pb-2'>Добавление нового оффера</h3>
+            <form action='/' method='post' id='offer-new' class='text-center'>
+                <label for="offer-new__name" class='fw-bolder w-50 text-start ps-2'>Имя:</label><br>
+                <input type="text" name="name" class='w-50 mb-2 border' id="offer-new__name"><br>
+                <label for="offer-new__price" class='fw-bolder w-50 text-start ps-2'>Цена:</label><br>
+                <input type="number" name="price" class='w-50 mb-2 border' id="offer-new__price"><br>
+                <label for="offer-new__name" class='fw-bolder w-50 text-start ps-2'>URL:</label><br>
+                <input type="url" name="" class='w-50 mb-2 border' id="offer-new__url"><br>
+                <label for="offer-new__name" class='fw-bolder w-50 text-start ps-2'>Тема:</label><br>
+                <select name="theme" class='w-50 mb-2 border' id="offer-new__theme">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select><br>
+                <input type="submit" class='btn btn-outline-dark' value='Добавить'>
+            </form>
+        </article>
 
-                        @foreach ($user->advertiser_products->all() as $product)
+        <article class="mt-4">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-4 bg-white border-b border-gray-200 text-center">
+                        <h3 class='h3 pb-3 fw-bolder'>Список офферов</h3>
+                        <table class='table w-75 mx-auto fs-4 w-100'>
                             <tr>
-                                <td class='fw-bolder'>{{$product->offer->name}}</td>
-                                <td>{{$product->price}} </td>
-                                @if ($product->status===1)
-                                <td class='fw-bolder text-success'>активен</td> 
-                                @else
-                                <td class='fw-bolder text-danger'>неактивен</td> 
-                                @endif
-                                <td>{{$product->links->count()}} </td>
+                                <th scope="col">Оффер</th>
+                                <th scope="col">Цена</th>
+                                <th scope="col">Статус</th>
+                                <th scope="col">Подписчики</th>
                             </tr>
-                        @endforeach
-                    </table>
-                </section>
+
+                            @foreach ($user->advertiser_products->all() as $product)
+                                <tr class='{{$product->id}}'>
+                                    <td class='fw-bolder'>{{$product->offer->name}}</td>
+                                    <td>{{$product->price}} </td>
+                                    <td class='p-0'>
+                                        <div class='form-switch p-0 h-100'>
+                                            @if ($product->status===1)
+                                            <input type="checkbox" name="status" class='form-check-input mx-auto' checked> 
+                                            @else
+                                            <input type="checkbox" name="status" class='form-check-input mx-auto'>
+                                            @endif
+                                        </div>
+                                    </td>
+                                    <td>{{$product->links->count()}} </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
+        </article>
+    </section>
 </x-app-layout>
