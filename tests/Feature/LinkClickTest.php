0d6400bc34609c13c2df665a1e0954bd46ce60c5
Aladser <aladser@gmail.com>
2023-09-29 00:40:37 +0900

    обновление тестов кликов

--- tests/Feature/LinkClickTest.php
+++ tests/Feature/LinkClickTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Offer;
+use App\Models\OfferClick;
 use App\Models\OfferSubscription;
 use App\Models\Advertiser;
 use App\Models\Webmaster;
@@ -15,34 +16,22 @@ class LinkClickTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testAdvertiserOffers()
+    public function testAdvertiserClicks()
     {
         if (User::count() === 0) {
             $this->seed();
         }
 
-        echo "  офферы рекламщиков\n";
-        for ($i=1; $i<4; $i++) {
-            $advertiser = Advertiser::find($i);
-            echo $advertiser->user->name . ': ' . $advertiser->offers->count() . " оффера\n";
-        }
-        echo "  расходы рекламщиков\n";
-        
+        $advertiser = Advertiser::find(1);
+        echo "клики и расходы рекламщика {$advertiser->user->name}:\n";
 
-        $this->assertDatabaseCount('offer_subscriptions', 6);
-    }
-
-    public function testMasterSubscriptions()
-    {
-        if (User::count() === 0) {
-            $this->seed();
+        foreach ($advertiser->offers as $offer) {
+            echo "{$offer->name}. цена:{$offer->price} переходов:{$offer->clicks->count()} сумма:";
+            echo $offer->clicks->count() * $offer->price . "\n";
         }
-
-        echo "офферы вебмастеров\n";
-        for ($i=1; $i<4; $i++) {
-            $webmaster = Webmaster::find($i);
-            echo $webmaster->user->name . ': ' . $webmaster->subscriptions->count() . " подписок\n";
-        }
+        $table = OfferClick::join('offers','offers.id','=','offer_clicks.offer_id');
+        echo "Итог. переходов:{$table->where('advertiser_id', 1)->count()} сумма:";
+        echo $table->sum('price')."\n";
 
         $this->assertDatabaseCount('offer_subscriptions', 6);
     }
