cb99fe10ee752cf907cad70e7f571bb0c0679b39
Aladser <aladser@gmail.com>
2023-09-29 23:15:49 +0900

    обновление верстки

--- resources/views/pages/webmaster.blade.php
+++ resources/views/pages/webmaster.blade.php
@@ -13,48 +13,46 @@
 
     <div class="py-12">
         <div class="w-75 mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <section class="bg-white m-0 d-flex justify-content-center mb-3 mt-3">
-                    <a href="/offer/statistics" class='btn btn-outline-dark'>Статистика офферов</a><br>
-                </section>
+            <section class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <p class='text-center p-3'>
+                    <a href="/offer/statistics" class='btn btn-outline-dark'>Статистика офферов</a>
+                </p>
+                <p class='h3 text-center fs-5'>Для деактивации оффера перетащите его в правую колонку</p>
+                <p class='h3 text-center fs-5'>Для активации оффера переташите его в левую колонку</p>
+                <p id='prg-error' class='fw-bolder pt-4 fs-4 text-center text-danger'></p>
 
-                <section>
-                    <p class='h3 text-center fs-5'>Для деактивации оффера перетащите его в правую колонку</p>
-                    <p class='h3 text-center fs-5'>Для активации оффера переташите его в левую колонку</p>
-                    <p id='prg-error' class='fw-bolder pt-4 fs-4 text-center text-danger'></p>
-                    <section class="bg-white border-b border-gray-200 m-0 d-flex justify-content-between text-center">
-                        <!-- офферы-подписки пользователя -->
-                        <article class='w-50 d-inline-block m-0 p-3 border-2 border-top-0 border-start-0 border-bottom-0'>
-                            <h4 class='h4 fw-bolder'>Ваши офферы</h4>
-                            <article class='w-100 h-100 table-items subscriptions' id='list-subscriptions'>
-                                @foreach ($subscriptions->get() as $subscription)
-                                    @if ($subscription->offer->status == 1)
-                                        <article id="subscription-{{$subscription->offer->id}}" class='border-666 mb-1 rounded cursor-pointer subscriptions__item' draggable='true'>
-                                            <p class='fw-bolder'>{{$subscription->offer->name}}</p>
-                                            <p>цена: {{$subscription->offer->price * $incomePercent}} р. за переход</p>
-                                            <p>тема: {{$subscription->offer->theme->name}}</p>
-                                        </article>
-                                    @endif
-                                @endforeach
-                            </article>
-                        </article>
-
-                        <!-- доступные активные офферы -->
-                        <article class='w-50 d-inline-block m-0 p-3'>
-                            <h4 class='h4 fw-bolder'>Доступные офферы</h4>
-                            <article class='w-100 h-100 table-items offers'  id='list-active-offers'>
-                                @foreach ($offers->get() as $offer)
-                                    <article id="offer-{{$offer->id}}" class='border-666 mb-1 rounded cursor-pointer bg-light offers__item' draggable='true'>
-                                        <p class='fw-bolder'>{{$offer->name}}</p>
-                                        <p>цена: {{$offer->price * $incomePercent}} р. за переход</p>
-                                        <p>тема: {{$offer->theme->name}}</p>
+                <section class="bg-white border-b border-gray-200 m-0 d-flex justify-content-between text-center">
+                    <!-- офферы-подписки пользователя -->
+                    <article class='w-50 d-inline-block m-0 p-3 border-2 border-top-0 border-start-0 border-bottom-0'>
+                        <h4 class='h4 fw-bolder'>Ваши офферы</h4>
+                        <article class='w-100 h-100 table-items subscriptions' id='list-subscriptions'>
+                            @foreach ($subscriptions->get() as $subscription)
+                                @if ($subscription->offer->status == 1)
+                                    <article id="subscription-{{$subscription->offer->id}}" class='border-666 mb-1 rounded cursor-pointer subscriptions__item' draggable='true'>
+                                        <p class='fw-bolder'>{{$subscription->offer->name}}</p>
+                                        <p>цена: {{$subscription->offer->price * $incomePercent}} р. за переход</p>
+                                        <p>тема: {{$subscription->offer->theme->name}}</p>
                                     </article>
-                                @endforeach
-                            </article>
+                                @endif
+                            @endforeach
                         </article>
-                    </section>
+                    </article>
+
+                    <!-- доступные активные офферы -->
+                    <article class='w-50 d-inline-block m-0 p-3'>
+                        <h4 class='h4 fw-bolder'>Доступные офферы</h4>
+                        <article class='w-100 h-100 table-items offers'  id='list-active-offers'>
+                            @foreach ($offers->get() as $offer)
+                                <article id="offer-{{$offer->id}}" class='border-666 mb-1 rounded cursor-pointer bg-light offers__item' draggable='true'>
+                                    <p class='fw-bolder'>{{$offer->name}}</p>
+                                    <p>цена: {{$offer->price * $incomePercent}} р. за переход</p>
+                                    <p>тема: {{$offer->theme->name}}</p>
+                                </article>
+                            @endforeach
+                        </article>
+                    </article>
                 </section>
-            </div>
+            </section>
         </div>
     </div>
 </x-app-layout>
