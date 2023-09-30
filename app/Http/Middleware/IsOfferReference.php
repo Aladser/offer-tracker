c976a02edb4540d3a1fc83ceaaa450860dc05ce6
Aladser <aladser@gmail.com>
2023-09-30 22:37:42 +0900

    добавление переходов рефарельных ссылок в базу

--- app/Http/Middleware/IsOfferReference.php
+++ app/Http/Middleware/IsOfferReference.php
@@ -5,8 +5,9 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use App\Models\OfferSubscription;
+use App\Models\OfferClick;
 
-/** проверка наличия реферальной ссылки */
+/** отлавливает реферальную ссылку */
 class IsOfferReference
 {
     public function handle(Request $request, Closure $next)
@@ -14,12 +15,13 @@ class IsOfferReference
         $params = $request->all();
         if (array_key_exists('ref', $params)) {
             $refCode = $params['ref'];
-            $reference = OfferSubscription::where('refcode', $refCode)->first();
-            if (is_null($reference)) {
+            $subscription = OfferSubscription::where('refcode', $refCode)->first();
+            if (is_null($subscription)) {
                 return redirect('page404');
             } else {
-                dd($reference->toArray());
-                return redirect($reference->offer->url);
+                // зафиксировать факт перенаправления
+                OfferClick::add($subscription->follower->id, $subscription->offer->id);
+                return redirect($subscription->offer->url);
             }
         }
         return $next($request);
--- app/Models/OfferClick.php
+++ app/Models/OfferClick.php
@@ -23,4 +23,12 @@ class OfferClick extends Model
     {
         return $this->belongsTo(Webmaster::class, 'webmaster_id', 'id');
     }
+
+    public static function add($webmasterId, $offerId)
+    {
+        $click = new OfferClick();
+        $click->webmaster_id = $webmasterId;
+        $click->offer_id = $offerId;        
+        return $click->save() ? 1 : 0;
+    }
 }
