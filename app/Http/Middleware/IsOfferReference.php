3aa5e40b602336c03353b7316f69d45769814e94
Aladser <aladser@gmail.com>
2023-09-30 23:22:52 +0900

    добавлено логирование переходов реферальных ссылок

--- app/Http/Middleware/IsOfferReference.php
+++ app/Http/Middleware/IsOfferReference.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Services\OfferService;
 use App\Models\OfferSubscription;
 use App\Models\OfferClick;
 
@@ -13,14 +15,23 @@ class IsOfferReference
     public function handle(Request $request, Closure $next)
     {
         $params = $request->all();
+
         if (array_key_exists('ref', $params)) {
+            $logChannel = Log::build([
+                'driver' => 'single',
+                'path' => storage_path('logs/offer_click.log'),
+              ]);
+            $time = OfferService::getDate();
+
             $refCode = $params['ref'];
             $subscription = OfferSubscription::where('refcode', $refCode)->first();
             if (is_null($subscription)) {
+                Log::stack(['slack', $logChannel])->info("$time: переход по ссылке {$request->path()}?ref=$refCode завершился с ошибкой");
                 return redirect('page404');
             } else {
                 // зафиксировать факт перенаправления
                 OfferClick::add($subscription->follower->id, $subscription->offer->id);
+                Log::stack(['slack', $logChannel])->info("$time: переход по ссылке {$request->path()}?ref=$refCode успешен");
                 return redirect($subscription->offer->url);
             }
         }
