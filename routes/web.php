dd503ee0392240c18f71f3cd9d8d5fa360c63d14
Aladser <aladser@gmail.com>
2023-10-09 16:55:24 +0900

    обновление роутинга

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ use App\Services\SubscriptionService;
 use App\Http\Controllers\OfferThemeController;
 use App\Http\Controllers\SystemOptionController;
 
+// аутентификация
+require __DIR__.'/auth.php';
+
+
 // страница реферальных ссылок
 Route::get('/', function() {
         $subscriptions = OfferSubscription::join('offers','offer_subscriptions.offer_id', '=', 'offers.id')
@@ -25,33 +29,37 @@ Route::get('/', function() {
 Route::get('/dashboard', DashboardController::class)
     ->middleware(['auth'])
     ->name('dashboard');
-// пользователи
-Route::resource('/users', UserController::class)
-    ->except(['show', 'create', 'edit', 'update'])
-    ->middleware(['auth']);
-Route::post('/users/status', [UserController::class, 'status']);
-// аутентификация
-require __DIR__.'/auth.php';
-// контроллер офферов
-Route::resource('/offer', OfferController::class)
-    ->except(['index', 'show', 'edit', 'update'])
-    ->middleware(['auth']);
-// установка статуса оффера
-Route::post('/offer/status', [OfferController::class, 'status']);
 // статистика офферов по переходам и деньгам
 Route::get('/offer/statistics', [StatisticController::class, 'index'])
     ->middleware(['auth'])
     ->name('offer.statistics');
-// подписка-отписка вебмастеров на офферы
-Route::post('/offer/subscribe', [SubscriptionService::class, 'subscribe']);
-Route::post('/offer/unsubscribe', [SubscriptionService::class, 'unsubscribe']);
-// контроллер тем офферов
-Route::resource('/offer-theme', OfferThemeController::class)
-    ->except(['show', 'create', 'edit', 'update'])
-    ->middleware(['auth']);
 // подмена csrf
 Route::get('/wrong-uri', fn() => view('wrongcsrf'));
 // выключен JS
 Route::get('/noscript', fn() => view('noscript'));
 // установка комиссии
+
+
+// пользователи
+Route::resource('/users', UserController::class)
+    ->except(['show', 'create', 'edit', 'update'])
+    ->middleware(['auth']);
+// контроллер офферов
+Route::resource('/offer', OfferController::class)
+    ->except(['index', 'show', 'edit', 'update'])
+    ->middleware(['auth']);
+// контроллер тем офферов
+Route::resource('/offer-theme', OfferThemeController::class)
+    ->except(['show', 'create', 'edit', 'update'])
+    ->middleware(['auth']);
+
+
+// установитьт статус пользователя (активен-неактивен)
+Route::post('/users/status', [UserController::class, 'status']);
+// установка статуса оффера (активен-неактивен)
+Route::post('/offer/status', [OfferController::class, 'status']);
+// подписка-отписка вебмастеров на офферы
+Route::post('/offer/subscribe', [SubscriptionService::class, 'subscribe']);
+Route::post('/offer/unsubscribe', [SubscriptionService::class, 'unsubscribe']);
+// комиссия 
 Route::post('/commission', [SystemOptionController::class, 'store']);
