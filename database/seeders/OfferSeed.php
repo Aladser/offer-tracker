f1cb9ff7a09239afe51d462a4110754f68645439
Aladser <aladser@gmail.com>
2023-10-12 09:05:22 +0900

    обновление сидов

--- database/seeders/OfferSeed.php
+++ database/seeders/OfferSeed.php
@@ -10,54 +10,62 @@ class OfferSeed extends Seeder
     public function run()
     {
         Offer::create([
-            'name'=>'Оффер 1',
+            'name'=> 'Github',
+            'url' => 'https://github.com/Aladser',
             'theme_id'=>1,
             'advertiser_id' => 1,
             'price' => random_int(1, 10),
             'status' => 1
         ]);
         Offer::create([
-            'name'=>'Оффер 2',
+            'name'=>'Django',
+            'url' => 'https://metanit.com/python/django/3.1.php',
             'theme_id'=>2,
             'advertiser_id' => 2,
             'price' => random_int(1, 10),
             'status' => 1
         ]);
         Offer::create([
-            'name'=>'Оффер 3',
+            'name'=>'Laravel 8',
             'theme_id'=>3,
             'advertiser_id' => 3,
             'price' => random_int(1, 10)
         ]);
         Offer::create([
-            'name'=>'Оффер 4', 'theme_id'=>1, 
+            'name'=>'Laravel 10', 
+            'url' => 'https://laravel.com/docs/10.x',
+            'theme_id'=>1, 
             'advertiser_id' => 1, 
             'price' => random_int(1, 10), 
             'status' => 1
         ]);
         Offer::create([
-            'name'=>'Оффер 5', 
+            'name'=>'БЭМ', 
+            'url' => 'https://ru.bem.info/methodology/quick-start/',
             'theme_id'=>2, 
             'advertiser_id' => 1, 
             'price' => random_int(1, 10),
             'status' => 0
         ]);
         Offer::create([
-            'name'=>'Оффер 6', 
+            'name'=>'Boostrap',
+            'url' => 'https://bootstrap-4.ru/docs/5.3/getting-started/introduction/',
             'theme_id'=>2, 
             'advertiser_id' => 2, 
             'price' => random_int(1, 10), 
             'status' => 1
         ]);
         Offer::create([
-            'name'=>'Оффер 7', 
+            'name'=>'Яндекс', 
+            'url' => 'https://ya.ru/',
             'theme_id'=>1, 
             'advertiser_id' => 2, 
             'price' => random_int(1, 10), 
             'status' => 0
         ]);
         Offer::create([
-            'name'=>'Оффер 8', 
+            'name'=>'Google', 
+            'url' => 'https://www.google.com/?hl=RU',
             'theme_id'=>1, 
             'advertiser_id' => 3, 
             'price' => random_int(1, 10), 
